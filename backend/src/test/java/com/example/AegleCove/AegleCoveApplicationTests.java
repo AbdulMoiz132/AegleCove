3501b20ed78f0afb4cc249a9ef60b1b364e285e3
AbdulMoiz132 <moizmemon457@gmail.com>
2024-11-16 23:47:42 +0500

    Initial setup

---
+++ backend/src/test/java/com/example/AegleCove/AegleCoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AegleCove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegleCoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
